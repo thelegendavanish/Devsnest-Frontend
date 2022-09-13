5b25f785f55913d01dc4846912082872668e9f53
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-09-13 21:30:07 +0530

    Create JS Function Hello.js

---
+++ JS Function Hello.js
@@ -0,0 +1,10 @@
+// Make a funciton named hello that should return "hello world" 
+// Code here
+function hello(a) {
+  return "hello world"
+}
+  // code to be executed
+
+
+// don't touch this code
+try { module.exports = { hello } } catch (e) {}
