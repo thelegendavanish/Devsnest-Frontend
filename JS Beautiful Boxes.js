73d823e0c0bca5edfa8bdacefeefa83578adba10
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-09-24 14:44:35 +0530

    Create JS Beautiful Boxes.js

---
+++ JS Beautiful Boxes.js
@@ -0,0 +1,16 @@
+let list5 = document.getElementsByClassName("box")[0];
+list5.style.height = "100px";
+list5.style.backgroundColor = "darkseagreen";
+list5.style.width = "100px";
+let list6 = document.getElementsByClassName("box")[1];
+list6.style.height = "100px";
+list6.style.backgroundColor = "darkseagreen";
+list6.style.width = "100px";
+let list7 = document.getElementsByClassName("box")[2];
+list7.style.height = "100px";
+list7.style.backgroundColor = "darkseagreen";
+list7.style.width = "100px";
+let list8 = document.getElementsByClassName("box")[3];
+list8.style.height = "100px";
+list8.style.backgroundColor = "darkseagreen";
+list8.style.width = "100px";
