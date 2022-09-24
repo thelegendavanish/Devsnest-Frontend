d8873bcd22576310ef23baccc9e47090cb99c249
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-09-24 14:40:40 +0530

    Create JS Datatypes.js

---
+++ JS Datatypes.js
@@ -0,0 +1,15 @@
+/* 
+You are given three variables: name, age and isEmployeed
+add your name as string datatype in variable name
+add your age in variable age as number (must be greater than 0)
+add your employment status as a boolean in isEmployeed 
+*/
+
+var name, age, isEmployeed
+/* start code here: */
+name="ABC";
+age=18;
+isEmployeed=true;
+
+/* end here */
+try { module.exports = { name, age, isEmployeed } } catch(e) {}
