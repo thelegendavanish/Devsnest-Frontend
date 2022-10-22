096290a416e6e1252018fb6f01cfcff85e88ce78
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-10-22 22:58:48 +0530

    Create JS Create Counter.js

---
+++ JS Create Counter.js
@@ -0,0 +1,17 @@
+/* Create a class named Counter with non-static property count and a member function increment
+& -- Read the given instructions -- */
+
+/* Code from here */
+
+class Counter{
+  constructor(count){
+   this.count=0;
+  }
+  increment(){
+    this.count++;
+    return count;
+  }
+}
+/* to here */
+
+try { module.exports = { Counter } } catch (e) { }
