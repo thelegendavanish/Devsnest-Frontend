ed0ef5211b343859b90c113778435837d65bbcb3
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-10-22 22:50:52 +0530

    Create JS Default Parameter Value.js

---
+++ JS Default Parameter Value.js
@@ -0,0 +1,21 @@
+/* Create a function getPerson, that have name, age, isEmployed
+assign isEmployed a default value false
+and return the details in form of a javascript object like given in the example in instructions */
+
+/* Code from here */
+function getPerson(){
+  var name="";
+  var age=0;
+  var isEmployed="";
+  //return getPerson();
+  return {
+   name: "a",
+   age: 5,
+   isEmployed: true
+}
+}
+
+
+/* to here */
+
+try { module.exports = { getPerson } } catch (e) { } 
