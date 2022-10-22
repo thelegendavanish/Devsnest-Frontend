a861ac29e29b92e710b71e46d9fe649ba01a780d
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-10-22 22:50:04 +0530

    Create JS Arrow Functions.js

---
+++ JS Arrow Functions.js
@@ -0,0 +1,10 @@
+// Create an arrow function devsnest 
+// that returns a string `Hello World!`
+// Note: don't use function and explicit return keywords
+
+
+// code here
+let devsnest = "";
+devsnest = () => "Hello World!";
+
+try { module.exports = { devsnest } } catch(e) {}
