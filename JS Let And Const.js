12e6e5b13233248af05cad3fc5c5cd6e77b80a84
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-12-02 23:32:32 +0530

    Create JS Let And Const.js

---
+++ JS Let And Const.js
@@ -0,0 +1,13 @@
+{
+    // Write your from here
+    var name="honry";
+    let age=1;
+    let isEmployeed=true;
+  let URL='http://localhost:3000'
+
+    // to here
+
+  
+  // dont remove this line
+  try { module.exports = { name, age, isEmployeed, URL } } catch (e) { }
+}
