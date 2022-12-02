ac604c5a0a7349d6eb611ef65adc6867d26e384a
Avanish Kumar ✪ <107942286+thelegendavanish@users.noreply.github.com>
2022-12-02 23:30:00 +0530

    Create React Functional Component JSX.jsx

---
+++ React Functional Component JSX.jsx
@@ -0,0 +1,12 @@
+// In this task, you have to make a
+// React Functional Component (Devsnest) that takes
+// heading as prop and render that heading with the help of JSX
+// inside the functional component 
+// and export the Functional Component as default
+export default function Devsnest({heading}){
+  return(
+    <h1>{heading}</h1>
+  )
+}
+
+// code here
